8af1b2770c09efdf715c1df375fb0adc9d28199a
SUSHIL PANDEY <95053082+codERSunny812@users.noreply.github.com>
2023-10-04 20:27:05 +0000

    Recursion in java

---
+++ Collection framework i/Collection/ArrayList.java
@@ -0,0 +1,13 @@
+package Collection framework ii ;
+import java.util.*;
+
+public interface ArrayList {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        System.out.println(list);
+
+
+    }
+}
