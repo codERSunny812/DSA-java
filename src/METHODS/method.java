7deca67c25aa267a663af93f96cc723f5d865468
codersunny812 <sengersunny448@gmail.com>
2024-07-06 22:03:35 +0530

    methods in java

--- src/METHODS/method.java
+++ src/METHODS/method.java
@@ -1,4 +1,10 @@
 package METHODS;
 
 public class method {
+    static void greet(){
+        System.out.println("hello from a new function ");
+    }
+    public static void main(String[] args) {
+        greet();
+    }
 }
