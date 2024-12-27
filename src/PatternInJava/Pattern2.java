87eee99dd58627b345c4138504287d0ad1ebee4d
codersunny812 <sengersunny448@gmail.com>
2024-12-27 16:07:48 +0530

    Pattern in java

--- src/PatternInJava/Pattern2.java
+++ src/PatternInJava/Pattern2.java
@@ -3,8 +3,8 @@ package PatternInJava;
 public class Pattern2 {
     public static void main(String[] args) {
 //      hollow rectangle
-        int m=6;
-        int n =6;
+        int m=4;
+        int n =4;
         for (int i = 1; i <=m ; i++) {
             for (int j = 1; j <=n ; j++) {
                 if(i==1 || j== 1 || i== m || j==n){
