5916b3713a91f9b8d635abb6f9d2afb76763cea1
codersunny812 <sengersunny448@gmail.com>
2024-12-28 20:30:21 +0530

    Variable in java

--- src/JavaBasic/Variables.java
+++ src/JavaBasic/Variables.java
@@ -1,14 +1,33 @@
 package JavaBasic;
 
 public class Variables {
+    static int z = 12; // Static variable
+    public int h = 12; // Instance variable
+
     public static void main(String[] args) {
         System.out.println("variables in java");
 
-        //output in java
-        System.out.println("this is how we print output in java");
+        // Static variable
+        System.out.println("Static variable z: " + z);
 
-//        variale decleration in java
-        int number = 21;
-        System.out.println(number);
+        // Call a static method
+        sum(z);
+
+        // Create an instance to access the instance variable
+        Variables obj = new Variables();
+        System.out.println("Instance variable h: " + obj.h);
+
+        // Local variable demonstration
+        int a = 10; // Local variable
+        if (a > 5) {
+            int b = 5; // Local variable inside block
+            System.out.println("Local variable b: " + b);
+            a += b;
+        }
+        System.out.println("Local variable a after modification: " + a);
+    }
+
+    public static void sum(int num) {
+        System.out.println("Sum of num and z: " + (num + z));
     }
 }
