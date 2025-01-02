9e2aff8772635db377f32df06879b015e2eab4db
codersunny812 <sengersunny448@gmail.com>
2025-01-02 23:27:14 +0530

    Data Type in java

--- src/JavaBasic/DataType.java
+++ src/JavaBasic/DataType.java
@@ -5,13 +5,16 @@ public class DataType {
 //        data type in java
 
         // Primitive Data Types
+//        These data type are stored in the stack memory
         byte b = 10;
         int age = 25;
         double salary = 50000.75;
         char grade = 'A';
         boolean isJavaFun = true;
+        float percantage = 75.5f;
 
         // Non-Primitive Data Types
+//These are stored in the heap memory and the refrence are stored in the stack memory
         String name = "Alice";
         int[] marks = {85, 90, 78};
 
@@ -22,6 +25,7 @@ public class DataType {
         System.out.println("Grade: " + grade);
         System.out.println("Is Java Fun? " + isJavaFun);
         System.out.println("Name: " + name);
+        System.out.println("percantage:"+percantage);
         System.out.println("Marks: " + marks[0] + ", " + marks[1] + ", " + marks[2]);
 
     }
